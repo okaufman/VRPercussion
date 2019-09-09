62ba7318b9b7192d517cbd628c91345fefe10f31
Luca Rava <luca.rava@stud.hslu.ch>
2019-09-09 15:32:43 +0200

    spotLight which is selectable with keyboard

---
+++ Assets/Scripts/Highlight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Highlight : MonoBehaviour
+{
+    public Light spotLight;
+    public Light[] noSpotLight;
+    public KeyCode keyCode;
+
+    private void Start()
+    {
+        spotLight.intensity = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(keyCode))
+        {
+            spotLight.intensity = 1;
+            foreach(Light lights in noSpotLight)
+            {
+                lights.intensity = 0;
+            }
+        }
+    }
+}
